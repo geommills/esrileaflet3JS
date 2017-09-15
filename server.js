81a994dbc124033a395971223e4d13e9ed3264eb
mikemillspdx <mikemillspdx@gmail.com>
2017-09-15 15:35:58 -0700

    syncup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var siteLoader = require('./scripts/server/lib/controllers/siteLoader.js');
 
 var server = restify.createServer();
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
 server.use(restify.queryParser());
 server.get('/sendEmail', siteLoader.sendEmail);
 server.get('/getToken', siteLoader.getMapBoxAPIToken);
